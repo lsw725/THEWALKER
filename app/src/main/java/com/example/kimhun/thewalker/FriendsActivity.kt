b46054450da007116ab3ff30a6eeda296e1464cf
hihi5345 <hihi5345@naver.com>
2017-10-14 17:06:26 +0900

    친구추가 및 로그인 시 User DB에 저장

--- app/src/main/java/com/example/kimhun/thewalker/FriendsActivity.kt
+++ app/src/main/java/com/example/kimhun/thewalker/FriendsActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class FriendsActivity : Activity() {
 
     private lateinit var friendsBtn : Button
     private lateinit var friendsID : EditText
+    private lateinit var database : DatabaseReference
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,14 @@ class FriendsActivity : Activity() {
         var adapter = FriendsListViewAdapter()
         friendsID = findViewById(R.id.add_friends) as EditText
 
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+        val userId = currentUser!!.email
+        var index = userId!!.indexOf("@")
+        var path = userId.substring(0,index)
+
+
+        database = FirebaseDatabase.getInstance().reference
 
         friendsListView.adapter = adapter
 
@@ -28,8 +41,8 @@ class FriendsActivity : Activity() {
 
         friendsBtn = findViewById(R.id.addFriend) as Button
         friendsBtn.setOnClickListener{
-
-
+            database.child("friends").child(path).push().setValue(friendsID.text.toString())
+            friendsID.setText("")
         }
     }
 }
--- app/src/main/java/com/example/kimhun/thewalker/StartActivity.kt
+++ app/src/main/java/com/example/kimhun/thewalker/StartActivity.kt
@@ -18,6 +18,8 @@ import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class StartActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
@@ -29,6 +31,7 @@ class StartActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
 
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mGoogleApiClient: GoogleApiClient
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -102,11 +105,21 @@ class StartActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
     private fun signIn() {
         val signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
         startActivityForResult(signInIntent, RC_SIGN_IN)
+
+
     }
     // [END signin]
 
     private fun updateUI(user: FirebaseUser?) {
         if (user != null) {
+
+            val currentUser = mAuth.currentUser
+            val userId = currentUser!!.email
+            var index = userId!!.indexOf("@")
+            var path = userId.substring(0,index)
+            database = FirebaseDatabase.getInstance().reference
+            database.child("user").child(path).setValue(0)
+
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
