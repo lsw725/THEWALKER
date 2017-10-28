d2d88b6ce9451723bafb2c7f8c60828e7bc3b06b
hihi5345 <hihi5345@naver.com>
2017-10-28 16:36:52 +0900

    친구목록에 현재 저장된 친구 출력 완료 [수정요구사항] 친구 실시간 점수 가져오기 친구들 점수순으로 정렬하기 친구목록을 켰을때 바로 친구들 이름이 뜨는게 아니라 edittext를 한두번 클릭해야 친구목록이 뜨는 문제 해결

--- app/src/main/java/com/example/kimhun/thewalker/FriendsActivity.kt
+++ app/src/main/java/com/example/kimhun/thewalker/FriendsActivity.kt
@@ -1,11 +1,13 @@
 package com.example.kimhun.thewalker
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
@@ -30,14 +32,28 @@ class FriendsActivity : Activity() {
         var index = userId!!.indexOf("@")
         var path = userId.substring(0,index)
         var userArray = ArrayList<String>()
+        var friendsArray = ArrayList<String>()
 
         database = FirebaseDatabase.getInstance().reference
 
-        friendsListView.adapter = adapter
+        val friendsListener = object : ValueEventListener{
+            override fun onDataChange(dataSnapshot : DataSnapshot?) {
+                friendsArray.clear()
+                for(snapshot in dataSnapshot!!.children) {
+                    var order = snapshot.key
+                    friendsArray.add(order.toString())
+                    Log.d("helpme",order.toString())
+                    adapter.addItem(1,order.toString(),1)
+                }
+            }
 
-        adapter.addItem(1,"KIM",1555123)
-        adapter.addItem(2,"Lee",12323)
-        adapter.addItem(3,"Park",123)
+            override fun onCancelled(dataSnapshot: DatabaseError?) {
+
+            }
+        }
+
+        var currentFriendRef = FirebaseDatabase.getInstance().getReference("/friends/" + path)
+        currentFriendRef.addListenerForSingleValueEvent(friendsListener)
 
         friendsBtn = findViewById(R.id.addFriend) as Button
         friendsBtn.setOnClickListener{
@@ -56,22 +72,36 @@ class FriendsActivity : Activity() {
                 }
             }
 
+            var alertDialogBuilder : AlertDialog.Builder = AlertDialog.Builder(this)
+
             var currentRef = FirebaseDatabase.getInstance().getReference("/user")
             currentRef.addListenerForSingleValueEvent(postListener)
             for(user in userArray) {
                 if (friendsID.text.toString() == path) {
                     Log.w("FriendsActivity", "Don't add self for friend")
+                    alertDialogBuilder.setTitle("친 구 추 가")
+                    alertDialogBuilder.setMessage("자신을 친구추가 할 수 없습니다.")
+                            .setCancelable(true)
+                    alertDialogBuilder.show()
                     friendsID.setText("")
                     break
                 } else if(user == friendsID.text.toString()) {
                     database.child("friends").child(path).child(friendsID.text.toString()).setValue(friendsID.text.toString())
+                    Toast.makeText(this, "친구추가가 완료됐습니다.",Toast.LENGTH_SHORT)
                     friendsID.setText("")
                     break
                 } else {
+                    if(user == userArray[userArray.size - 1]){
+                        alertDialogBuilder.setTitle("친 구 추 가")
+                        alertDialogBuilder.setMessage("해당 아이디의 사용자가 존재하지않습니다.")
+                                .setCancelable(true)
+                        alertDialogBuilder.show()
+                    }
                     Log.w("FriendsActivity","failed order")
                     friendsID.setText("")
                 }
             }
         }
+        friendsListView.adapter = adapter
     }
 }
